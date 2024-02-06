96aa8f0bfd3f50d9ecd2ac9b1976370c6f0c73b7
chooyen <130257510+chooyen23@users.noreply.github.com>
2024-02-06 15:51:25 +0800

    update password

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -19,7 +19,7 @@ st.write("This app uses 6 inputs to predict the species of penguin using "
 
 password_guess = st.text_input('What is the Password?') 
 
-if password_guess != 'streamlit_is_great': 
+if password_guess != st.secrets['password']: 
   st.stop() 
 
   
